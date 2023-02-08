8a1ea6daa81736995437dc9ab081060c2d89f635
[Gaston Mieulet] <[gfmieulet@gmail.com]>
2023-02-07 22:03:49 -0300

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,5 @@
-variable "app" {
-    default = "nginx"
-}
-variable "env" {
-    default = "develop"
-}
-variable "ami" {
-    default = "ami-0fe472d8a85bc7b0e"
-}
-variable "instance_type" {
-    default = "t2.micro"
-}
-variable "key" {
-    default = "temporal"
-}
+variable "app" {}
+variable "env" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key" {}
